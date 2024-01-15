bc487cca782fcee4efe08df6cda538742081ed7b
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2024-01-15 07:03:48 -0800

    Hot fix

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ dotenv.config({ path: "./.env" });
 const app = require("./app");
 
 //connect to DB
-connectDB();
-
+//
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
@@ -20,7 +19,9 @@ const server = app.listen(PORT, () => {
     chalk.blue(
       `server running in ${process.env.NODE_ENV} & listening on port ${process.env.PORT}`
     )
-  );
+  ); 
+  // console.log(`server running in development & listening on port 3000`
+  // );
 });
 
 process.on("unhandledRejection", (err) => {
